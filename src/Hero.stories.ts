150359276ae07af75afe18914da089887dc910fa
M Nindra Zaka <mnindrazaka@gmail.com>
2024-11-01 17:04:38 +0700

    feat: add props to change image source of hero component

--- src/Hero.stories.ts
+++ src/Hero.stories.ts
@@ -19,5 +19,6 @@ export const Default: Story = {
       label: "Learn More",
       onPress: fn(),
     },
+    imageSource: "https://images.unsplash.com/photo-1484863137850-59afcfe05386?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1771&q=80"
   },
 };
--- src/Hero.tsx
+++ src/Hero.tsx
@@ -12,6 +12,7 @@ export type HeroProps = {
     label: string;
     onPress: () => void;
   };
+  imageSource: string;
 };
 
 export const Hero = (props: HeroProps) => {
@@ -49,12 +50,8 @@ export const Hero = (props: HeroProps) => {
           width="100%"
           height="100%"
           aspectRatio={16 / 9}
-          defaultSource={{
-            uri: "https://images.unsplash.com/photo-1484863137850-59afcfe05386?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1771&q=80",
-          }}
-          source={{
-            uri: "https://images.unsplash.com/photo-1484863137850-59afcfe05386?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1771&q=80",
-          }}
+          defaultSource={{ uri: props.imageSource }}
+          source={{ uri: props.imageSource }}
         />
       </YStack>
     </XStack>
