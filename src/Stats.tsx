22c09ac8faa8c9a0fda28015210f1672ed3c851d
M Nindra Zaka <mnindrazaka@gmail.com>
2024-11-04 11:01:10 +0700

    feat: implement stats component

---
+++ src/Stats.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { H1, Paragraph, XStack, YStack } from "tamagui";
+
+export type StatsProps = {
+  title: string;
+  subtitle: string;
+  items: {
+    title: string;
+    subtitle: string;
+  }[];
+};
+
+export const Stats = (props: StatsProps) => {
+  return (
+    <YStack gap="$5" alignItems="center">
+      <YStack gap="$5" maxWidth={640}>
+        <H1 textAlign="center">{props.title}</H1>
+        <Paragraph size="$5" textAlign="center">
+          {props.subtitle}
+        </Paragraph>
+      </YStack>
+
+      <XStack gap="$10" $sm={{ flexDirection: "column" }}>
+        {props.items.map((item, index) => (
+          <>
+            <YStack alignItems="center">
+              <Paragraph size="$10" color="$blue10">
+                {item.title}
+              </Paragraph>
+              <Paragraph size="$5">{item.subtitle}</Paragraph>
+            </YStack>
+            {index < props.items.length - 1 ? (
+              <YStack
+                borderRightWidth="$0.5"
+                borderColor="$gray5"
+                $sm={{ display: "none" }}
+              />
+            ) : null}
+          </>
+        ))}
+      </XStack>
+    </YStack>
+  );
+};
