bd07e3a16473412150756f11c7054af564075d43
M Nindra Zaka <mnindrazaka@gmail.com>
2024-11-07 06:27:57 +0700

    feat: implement fixed navbar

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Paragraph, YStack } from "tamagui";
+import { Paragraph, ScrollView, YStack } from "tamagui";
 
 export type LayoutProps = {
   navbar: React.ReactNode;
@@ -11,16 +11,18 @@ export const Layout = (props: LayoutProps) => {
   return (
     <YStack height="100vh" justifyContent="space-between">
       <YStack>{props.navbar}</YStack>
-      <YStack
-        alignSelf="center"
-        width="100%"
-        maxWidth={1440}
-        paddingHorizontal="$8"
-        $xs={{ paddingHorizontal: "$5" }}
-      >
-        {props.main}
-      </YStack>
-      <YStack>{props.footer}</YStack>
+      <ScrollView>
+        <YStack
+          alignSelf="center"
+          width="100%"
+          maxWidth={1440}
+          paddingHorizontal="$8"
+          $xs={{ paddingHorizontal: "$5" }}
+        >
+          {props.main}
+        </YStack>
+        <YStack>{props.footer}</YStack>
+      </ScrollView>
     </YStack>
   );
 };
