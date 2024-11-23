1f87fc0390aea9260d54ac474cba8539327e04be
M Nindra Zaka <mnindrazaka@gmail.com>
2024-11-23 12:39:24 +0700

    chore: avoid export all

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,35 @@
-export * from "./Blog";
-export * from "./CallToAction";
-export * from "./CallToActionCentered";
-export * from "./Features";
-export * from "./Footer";
-export * from "./Hero";
-export * from "./HeroWithImageBackground";
-export * from "./HeroWithTopImage";
-export * from "./Layout";
-export * from "./LogoGrid";
-export * from "./LogoGridWithTile";
-export * from "./Navbar";
-export * from "./PageHeader";
-export * from "./Pricing";
-export * from "./Stats";
-export * from "./StatsWithHighlight";
-export * from "./Team";
-export * from "./TeamWithInlineHead";
-export * from "./Testimonial";
-export * from "./TestimonialCentered";
+export { Blog, BlogProps } from "./Blog";
+export { CallToAction, CallToActionProps } from "./CallToAction";
+export {
+  CallToActionCentered,
+  CallToActionCenteredProps,
+} from "./CallToActionCentered";
+export { Features, FeaturesProps } from "./Features";
+export { Footer, FooterProps } from "./Footer";
+export { Hero, HeroProps } from "./Hero";
+export {
+  HeroWithImageBackground,
+  HeroWithImageBackgroundProps,
+} from "./HeroWithImageBackground";
+export { HeroWithTopImage, HeroWithTopImageProps } from "./HeroWithTopImage";
+export { Layout, LayoutProps } from "./Layout";
+export { LogoGrid, LogoGridProps } from "./LogoGrid";
+export { LogoGridWithTile, LogoGridWithTileProps } from "./LogoGridWithTile";
+export { Navbar, NavbarProps } from "./Navbar";
+export { PageHeader, PageHeaderProps } from "./PageHeader";
+export { Pricing, PricingProps } from "./Pricing";
+export { Stats, StatsProps } from "./Stats";
+export {
+  StatsWithHighlight,
+  StatsWithHighlightProps,
+} from "./StatsWithHighlight";
+export { Team, TeamProps } from "./Team";
+export {
+  TeamWithInlineHead,
+  TeamWithInlineHeadProps,
+} from "./TeamWithInlineHead";
+export { Testimonial, TestimonialProps } from "./Testimonial";
+export {
+  TestimonialCentered,
+  TestimonialCenteredProps,
+} from "./TestimonialCentered";
