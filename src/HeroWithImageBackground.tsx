61e3ada8e979ca3ce8513fd6f29604134c198c55
M Nindra Zaka <mnindrazaka@gmail.com>
2025-01-20 22:01:01 +0700

    feat: make hero with background image always dark mode

--- src/HeroWithImageBackground.tsx
+++ src/HeroWithImageBackground.tsx
@@ -1,4 +1,4 @@
-import { Image, XStack, YStack } from "tamagui";
+import { Image, Theme, XStack, YStack } from "tamagui";
 import { PageHeader } from "./PageHeader";
 
 export type HeroWithImageBackgroundProps = {
@@ -19,52 +19,53 @@ export const HeroWithImageBackground = (
   props: HeroWithImageBackgroundProps
 ) => {
   return (
-    <XStack
-      position="relative"
-      $sm={{ padding: "$8" }}
-      $md={{ padding: "$10" }}
-      padding="$19"
-      themeInverse
-    >
-      <YStack
-        $sm={{ maxWidth: "100%" }}
-        $md={{ maxWidth: 360 }}
-        gap="$5"
-        maxWidth={480}
-      >
-        <PageHeader
-          title={props.title}
-          subtitle={props.subtitle}
-          primaryButton={props.primaryButton}
-          secondaryButton={props.secondaryButton}
-        />
-      </YStack>
-      <YStack
-        flex={1}
-        top={0}
-        bottom={0}
-        left={0}
-        right={0}
-        position="absolute"
-        zIndex={-999}
+    <Theme name="dark">
+      <XStack
+        position="relative"
+        $sm={{ padding: "$8" }}
+        $md={{ padding: "$10" }}
+        padding="$19"
       >
         <YStack
+          $sm={{ maxWidth: "100%" }}
+          $md={{ maxWidth: 360 }}
+          gap="$5"
+          maxWidth={480}
+        >
+          <PageHeader
+            title={props.title}
+            subtitle={props.subtitle}
+            primaryButton={props.primaryButton}
+            secondaryButton={props.secondaryButton}
+          />
+        </YStack>
+        <YStack
+          flex={1}
           top={0}
           bottom={0}
           left={0}
           right={0}
           position="absolute"
-          backgroundColor="$background"
-          opacity={0.5}
-          zIndex={999}
-        ></YStack>
-        <Image
-          width="100%"
-          height="100%"
-          defaultSource={{ uri: props.imageSource }}
-          source={{ uri: props.imageSource }}
-        />
-      </YStack>
-    </XStack>
+          zIndex={-999}
+        >
+          <YStack
+            top={0}
+            bottom={0}
+            left={0}
+            right={0}
+            position="absolute"
+            backgroundColor="$background"
+            opacity={0.5}
+            zIndex={999}
+          ></YStack>
+          <Image
+            width="100%"
+            height="100%"
+            defaultSource={{ uri: props.imageSource }}
+            source={{ uri: props.imageSource }}
+          />
+        </YStack>
+      </XStack>
+    </Theme>
   );
 };
